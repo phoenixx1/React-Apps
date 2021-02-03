e61ee109a8c22dacd15e9d9891828bbb3d4b36b2
Nishant Saxena <xphoenix1010@gmail.com>
2021-02-03 17:43:39 +0530

    Amazon Clone Initial commit

---
+++ amazon-clone/src/Checkout.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
+import Subtotal from "./Subtotal";
+
+function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/PrimeRewards/LP_Revamp/PC_Header_Banner._CB468631809_.jpg"
+          alt=""
+        />
+        <div>
+          <h2 className="checkout__title">Your Cart</h2>
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
